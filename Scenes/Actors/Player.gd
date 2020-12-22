57ec02c825033f9f0c0859440238b6e45b57d34e
Kacperacy <51511929+Kacperacy@users.noreply.github.com>
2020-12-22 13:41:13 +0100

    movement based on vectors

--- Scenes/Actors/Player.gd
+++ Scenes/Actors/Player.gd
@@ -1,33 +1,37 @@
 extends KinematicBody2D
 
-var motion = Vector2()
-var current_side = "Up" #Sprawdza w którą stronę odwrócony jest bohater
-var attack = false # Sprawdza czy bohater jest w trakcie ataku
-export var speed = 3
+var velocity = Vector2.ZERO
+var current_side = "Up" # Zmienna zawierająca stronę w którą odwrócony jest bohater
+var attack = false # Zmienna określająca czy bohater jest w trakcie ataku
+export var speed = 2
+var direction = Vector2()
 
 func _physics_process(delta):
-	motion = Vector2()
 	if Input.is_action_pressed("attack"):
 			$AnimationPlayer.play("Attack "+current_side)
 			attack = true
 			yield($AnimationPlayer,"animation_finished")
 			attack = false
 	elif !attack: #Jeżeli nie atakuje to
-		if Input.is_action_pressed("down"): #Idź w określoną stronę
-			motion.y = speed
-			$AnimationPlayer.play("Walk Down")
-		elif Input.is_action_pressed("up"):
-			motion.y = -speed
-			$AnimationPlayer.play("Walk Up")
-		elif Input.is_action_pressed("left"):
-			motion.x = -speed
-			$AnimationPlayer.play("Walk Left")		
-		elif Input.is_action_pressed("right"):
-			motion.x = speed
-			$AnimationPlayer.play("Walk Right")
-		else: # Lub stój 
-			$AnimationPlayer.play("Idle " + current_side)
-	move_and_collide(motion)
+		movement()
+		
+func movement():
+	direction = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	) # Określenie kierunku poruszania się
+	velocity = direction * speed
+	velocity = move_and_collide(velocity)
+	if direction == Vector2.ZERO:
+		$AnimationPlayer.play("Idle " + current_side)
+	elif direction.y < 0:
+		$AnimationPlayer.play("Walk Up")
+	elif direction.y > 0:
+		$AnimationPlayer.play("Walk Down")
+	elif direction.x < 0:
+		$AnimationPlayer.play("Walk Left")
+	elif direction.x > 0:
+		$AnimationPlayer.play("Walk Right")
 
-func _on_AnimationPlayer_animation_started(anim_name): #Pobierz ostatnią animację
-	current_side = anim_name.split(" ")[-1] #i weź z jej nazwy stronę
+func _on_AnimationPlayer_animation_started(anim_name): #Pobierz ostatnią wystartowaną animację
+	current_side = anim_name.split(" ")[-1] #i weź z jej nazwy stronę w którą jest zwrócony bohater
