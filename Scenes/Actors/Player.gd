502c9e209b318f86799fe4d72c6660994a59251d
F10k1 <67909049+f10k1@users.noreply.github.com>
2020-12-22 10:31:43 +0100

    Small Changes to Hero script

--- Scenes/Actors/Player.gd
+++ Scenes/Actors/Player.gd
@@ -1,31 +1,19 @@
 extends KinematicBody2D
 
 var motion = Vector2()
-var current_side = "Up"
-var attack = false
+var current_side = "Up" #Sprawdza w którą stronę odwrócony jest bohater
+var attack = false # Sprawdza czy bohater jest w trakcie ataku
 export var speed = 3
 
 func _physics_process(delta):
 	motion = Vector2()
-	if !attack:
-		if Input.is_action_pressed("attack"):
-			$AnimationPlayer.play("Attack " + current_side)
+	if Input.is_action_pressed("attack"):
+			$AnimationPlayer.play("Attack "+current_side)
 			attack = true
-			if current_side == "Up":
-				$AttackCollision/AttackUp.disabled = false
-			elif current_side == "Down":
-				$AttackCollision/AttackDown.disabled = false
-			elif current_side == "Left":
-				$AttackCollision/AttackLeft.disabled = false
-			elif current_side == "Right":
-				$AttackCollision/AttackRight.disabled = false
 			yield($AnimationPlayer,"animation_finished")
-			$AttackCollision/AttackUp.disabled = true
-			$AttackCollision/AttackDown.disabled = true
-			$AttackCollision/AttackLeft.disabled = true
-			$AttackCollision/AttackRight.disabled = true
 			attack = false
-		elif Input.is_action_pressed("down"):
+	elif !attack: #Jeżeli nie atakuje to
+		if Input.is_action_pressed("down"): #Idź w określoną stronę
 			motion.y = speed
 			$AnimationPlayer.play("Walk Down")
 		elif Input.is_action_pressed("up"):
@@ -37,9 +25,9 @@ func _physics_process(delta):
 		elif Input.is_action_pressed("right"):
 			motion.x = speed
 			$AnimationPlayer.play("Walk Right")
-		else:
+		else: # Lub stój 
 			$AnimationPlayer.play("Idle " + current_side)
 	move_and_collide(motion)
 
-func _on_AnimationPlayer_animation_started(anim_name):
-	current_side = anim_name.split(" ")[-1]
+func _on_AnimationPlayer_animation_started(anim_name): #Pobierz ostatnią animację
+	current_side = anim_name.split(" ")[-1] #i weź z jej nazwy stronę
