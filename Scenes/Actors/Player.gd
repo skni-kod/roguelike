37f3131011ec179f5a0308e64eff448f88d5e794
Kacperacy <51511929+Kacperacy@users.noreply.github.com>
2020-12-22 09:45:46 +0100

    attack collision

--- Scenes/Actors/Player.gd
+++ Scenes/Actors/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 const POSITION = Vector2(0,-1)
 
 var motion = Vector2()
-var anim = "Idle Up"
+var current_side = "Up"
 var attack = false
 
 func _ready():
@@ -12,7 +12,24 @@ func _ready():
 func _physics_process(delta):
 	motion = Vector2()
 	if !attack:
-		if Input.is_action_pressed("down"):
+		if Input.is_action_pressed("attack"):
+			$AnimationPlayer.play("Attack " + current_side)
+			attack = true
+			if current_side == "Up":
+				$AttackCollision/AttackUp.disabled = false
+			elif current_side == "Down":
+				$AttackCollision/AttackDown.disabled = false
+			elif current_side == "Left":
+				$AttackCollision/AttackLeft.disabled = false
+			elif current_side == "Right":
+				$AttackCollision/AttackRight.disabled = false
+			yield($AnimationPlayer,"animation_finished")
+			$AttackCollision/AttackUp.disabled = true
+			$AttackCollision/AttackDown.disabled = true
+			$AttackCollision/AttackLeft.disabled = true
+			$AttackCollision/AttackRight.disabled = true
+			attack = false
+		elif Input.is_action_pressed("down"):
 			motion.y = 5
 			$AnimationPlayer.play("Walk Down")
 		elif Input.is_action_pressed("up"):
@@ -24,16 +41,9 @@ func _physics_process(delta):
 		elif Input.is_action_pressed("right"):
 			motion.x = 5
 			$AnimationPlayer.play("Walk Right")
-		elif Input.is_action_pressed("attack"):
-			$AnimationPlayer.play("Attack "+anim)
-			attack = true
-			yield($AnimationPlayer,"animation_finished")
-			attack = false
 		else:
-			$AnimationPlayer.play("Idle "+anim)
+			$AnimationPlayer.play("Idle " + current_side)
 	move_and_collide(motion)
 
 func _on_AnimationPlayer_animation_started(anim_name):
-	anim = anim_name
-	anim = anim.split(" ")
-	anim = anim[1]
+	current_side = anim_name.split(" ")[-1]
