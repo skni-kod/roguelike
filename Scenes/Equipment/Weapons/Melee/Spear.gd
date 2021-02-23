ea6a9edd214b87af1df4476f16b808a292d647f1
Sqooby <michal.basznianin3@gmail.com>
2021-02-23 15:57:39 +0100

    Revert "Merge branch 'new-weapons' of https://github.com/skni-kod/roguelike into new-weapons"
    This reverts commit 367e970705c53619082cf3f165512be1cc0c933a.

--- Scenes/Equipment/Weapons/Melee/Spear.gd
+++ Scenes/Equipment/Weapons/Melee/Spear.gd
@@ -4,22 +4,16 @@ var mouse_position
 var attack = false
 var attack_vector = Vector2.ZERO
 export var attack_range = 15
-var timer #Cooldown pomiędzy atakami
-var damage #Obrażenia zadawane przez broń. Wartość pobierana z pliku
-var isWeaponReady=1 #Sprawdź czy broń jest gotowa do ataku
+onready var timer = $Timer
+onready var player_node := get_tree().get_root().find_node("Player", true, false)
+var damage = 20
+
+func _ready():
+	player_node.connect("attacked", self, "_on_Player_attacked")
 
 func _physics_process(delta):
-	if isWeaponReady==1: #Zmienia ustawienia timera i teksturę a także skaluje kolizję (_ready() nie działa)
-		timer.set_wait_time(0.2)
-		$WeaponSprite.texture = load("res://Assets/Loot/Weapons/spear.png")
-		#Ustaw wartość kolizji dla broni
-		$AttackCollision.scale.x = 1.15
-		$AttackCollision.scale.y = 0.5
-		$AttackCollision.position.x= 20
-		isWeaponReady = 0
 	if !attack:
 		mouse_position = get_local_mouse_position()
-		
 		if rotation < -PI:
 			rotation = PI + mouse_position.angle() * 0.1
 		elif rotation > PI:
@@ -27,11 +21,9 @@ func _physics_process(delta):
 		else:
 			rotation += mouse_position.angle() * 0.1
 		if rotation < -PI/2 or rotation > PI/2:
-			$WeaponSprite.scale.y = -1
-			$WeaponSprite.rotation_degrees=-90 #Obróć broń przodem do przeciwnika
+			$SwordSprite.scale.y = -1
 		else:
-			$WeaponSprite.scale.y = 1
-			$WeaponSprite.rotation_degrees=90 #Obróć broń przodem do przeciwnika
+			$SwordSprite.scale.y = 1
 
 func _on_Player_attacked():
 	if !attack:
@@ -39,22 +31,23 @@ func _on_Player_attacked():
 		attack_vector = Vector2(attack_range * cos(rotation), attack_range * sin(rotation))
 		position += attack_vector
 		if rotation < -PI/2 or rotation > PI/2:
-			$WeaponSprite.rotation_degrees = -90#-90
+			$SwordSprite.rotation_degrees = -90
 		else:
-			$WeaponSprite.rotation_degrees = 90#90
+			$SwordSprite.rotation_degrees = 90
 		$AttackCollision.disabled = false
 		timer.start()
 
 func _on_Timer_timeout():
 	position -= attack_vector
-	$WeaponSprite.rotation_degrees -= 5
+	$SwordSprite.rotation_degrees = 0
 	$AttackCollision.disabled = true
 	attack = false
 	timer.stop()
 
 func change_weapon(texture):
-	$WeaponSprite.texture = texture
+	$SwordSprite.texture = texture
 
-func _on_EquippedWeapon_body_entered(body):
+func _on_Axe_body_entered(body):
+	print('d')
 	if body.is_in_group("Enemy"):
 		body.get_dmg(damage)
