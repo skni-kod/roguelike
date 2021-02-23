43db048846fe0d52b95321aa1ed4c4486fedf219
Radzio17 <r.blok@protonmail.com>
2021-02-23 14:46:26 +0100

    Placeholder spear

--- Scenes/Equipment/Weapons/Melee/Spear.gd
+++ Scenes/Equipment/Weapons/Melee/Spear.gd
@@ -4,14 +4,18 @@ var mouse_position
 var attack = false
 var attack_vector = Vector2.ZERO
 export var attack_range = 15
-onready var timer = $Timer
+var timer #Cooldown pomiędzy atakami
 onready var player_node := get_tree().get_root().find_node("Player", true, false)
-var damage = 20
-
-func _ready():
-	player_node.connect("attacked", self, "_on_Player_attacked")
+var damage #Obrażenia zadawane przez broń. Wartość pobierana z pliku
+var isWeaponLoaded=0 #
 
 func _physics_process(delta):
+	if isWeaponLoaded==0: #Zmienia ustawienia timera i teksturę a także skaluje kolizję (_ready() nie działa)
+		timer.set_wait_time(0.2)
+		$WeaponSprite.texture = load("res://Assets/Loot/Weapons/spear.png")
+		$AttackCollision.scale.x = 1
+		$AttackCollision.scale.y = 1
+		isWeaponLoaded = 1
 	if !attack:
 		mouse_position = get_local_mouse_position()
 		if rotation < -PI:
@@ -21,9 +25,9 @@ func _physics_process(delta):
 		else:
 			rotation += mouse_position.angle() * 0.1
 		if rotation < -PI/2 or rotation > PI/2:
-			$SwordSprite.scale.y = -1
+			$WeaponSprite.scale.y = -1
 		else:
-			$SwordSprite.scale.y = 1
+			$WeaponSprite.scale.y = 1
 
 func _on_Player_attacked():
 	if !attack:
@@ -31,21 +35,21 @@ func _on_Player_attacked():
 		attack_vector = Vector2(attack_range * cos(rotation), attack_range * sin(rotation))
 		position += attack_vector
 		if rotation < -PI/2 or rotation > PI/2:
-			$SwordSprite.rotation_degrees = -90
+			$WeaponSprite.rotation_degrees = -90
 		else:
-			$SwordSprite.rotation_degrees = 90
+			$WeaponSprite.rotation_degrees = 90
 		$AttackCollision.disabled = false
 		timer.start()
 
 func _on_Timer_timeout():
 	position -= attack_vector
-	$SwordSprite.rotation_degrees = 0
+	$WeaponSprite.rotation_degrees = 0
 	$AttackCollision.disabled = true
 	attack = false
 	timer.stop()
 
 func change_weapon(texture):
-	$SwordSprite.texture = texture
+	$WeaponSprite.texture = texture
 
 func _on_Axe_body_entered(body):
 	print('d')
