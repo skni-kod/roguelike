3e3bc1fdf3e2467037996d1f51d4f3093013bb39
Kacper Maciołek <kacper.maciolek00@gmail.com>
2021-02-24 16:59:01 +0100

    map generation v2

--- Scenes/Levels/World.gd
+++ Scenes/Levels/World.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var borders = Rect2(1, 1, 38, 21)
+var borders = Rect2(1, 1, 50, 50)
 
 onready var tileMap = $Floor
 
@@ -9,9 +9,13 @@ func _ready():
 	generate_level()
 
 func generate_level():
-	var walker = Walker.new(Vector2(19, 11), borders)
-	var map = walker.walk(500)
+	var walker = Walker.new(Vector2(25, 25), borders)
+	var map = walker.walk(2000)
 	walker.queue_free()
 	for location in map:
-		tileMap.set_cellv(location, -1)
+		tileMap.set_cellv(location*2, 1)
+		tileMap.set_cellv(location*2+Vector2(1,0), 1)
+		tileMap.set_cellv(location*2+Vector2(0,1), 1)
+		tileMap.set_cellv(location*2+Vector2(1,1), 1)
+		
 	tileMap.update_bitmask_region(borders.position, borders.end)
