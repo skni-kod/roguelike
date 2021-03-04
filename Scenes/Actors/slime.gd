34221a29c0f0c8a5b95ab20feb89dfa6cc8cf836
Adrian <dobryemail0@wp.pl>
2021-03-04 22:03:18 +0100

    Slime bug fix

--- Scenes/Actors/slime.gd
+++ Scenes/Actors/slime.gd
@@ -63,10 +63,11 @@ func _on_Timer_timeout():
 			
 func get_dmg(dmg):
 	if health>0:
-		if player.position.x-self.position.x < 0:
-			self.position.x += 10
-		else:
-			self.position.x -= 10
+		if player != null:
+			if player.position.x-self.position.x < 0:
+				self.position.x += 10
+			else:
+				self.position.x -= 10
 		hp -= dmg
 		health = hp/max_hp*100
 		$AnimationPlayer.play("Hurt")
