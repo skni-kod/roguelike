d3642bd5d4cd6049f1f878c50549cb6c9e38dd4a
Mixu285 <remik285@gmail.com>
2021-03-05 21:20:33 +0100

    Random

---
+++ Scenes/Levels/Random.gd
@@ -0,0 +1,150 @@
+extends Node2D
+class_name Random
+signal started
+signal finished
+
+enum Cell {WALL, FLOOR, OBSTACLE}
+export(float, 0, 1) var ground_probability := 0.1
+
+var size := Vector2(10,10) #rozmiar planszy
+onready var _tile_map : TileMap = $TileMap # $TileMap == get.node("TileMap")
+var _rng := RandomNumberGenerator.new()
+var dl = size.y
+var szer = size.x
+var gen = 0
+var spr = 0
+var step = 6
+var hist = 0
+func _ready(): #funkcja wykonująca
+	_rng.randomize()
+	setup()
+	generate()
+	
+func setup(): #funkcja, która ustawia wymiary okna (można bardziej zmodyfikować)
+	OS.set_window_size(Vector2(1300,600))
+
+func generate(): #właściwa funkcja generująca z sygnałami
+	emit_signal("started")
+	generate_perimeter()
+	emit_signal("finished")
+
+func generate_perimeter(): #generacja pokoju
+	for n in 4:
+		for m in step:
+			for x in [szer,size.x + szer - 1]:
+				for y in range(dl, size.y + dl):
+					_tile_map.set_cell(x,y,_pick_random_texture(Cell.WALL)) #generacja ścian
+			for x in range (szer + 1, size.x + szer - 1):
+				for y in [dl, size.y + dl - 1]:
+					_tile_map.set_cell(x,y,_pick_random_texture(Cell.WALL)) #generacja ścian
+			for x in range (szer + 1, size.x + szer - 1):
+				for y in range(dl + 1, size.y + dl - 1):
+					var cell := get_random_tile(ground_probability) 
+					_tile_map.set_cell(x,y,cell) #generacja podłogi
+			if hist == 0:
+				if m < 2:
+					dl -= size.y
+				else:
+					gen = _rng.randi_range(0,10)
+					if gen >= 0 and gen <= 6:
+						dl -= size.y
+					elif gen > 6 and gen <= 8:
+						szer += size.x
+					elif gen > 8 and gen <= 10:
+						szer -= size.x
+			elif hist == 1:
+				if m < 2:
+					szer += size.x
+				else:
+					gen = _rng.randi_range(0,10)
+					if gen >= 0 and gen <= 6:
+						szer += size.x
+					elif gen > 6 and gen <= 8:
+						dl += size.y
+					elif gen > 8 and gen <= 10:
+						dl -= size.y
+			elif hist == 2:
+				if m < 2:
+					dl += size.y
+				else:
+					gen = _rng.randi_range(0,10)
+					if gen >= 0 and gen <= 6:
+						dl += size.y
+					elif gen > 6 and gen <= 8:
+						szer += size.x
+					elif gen > 8 and gen <= 10:
+						szer -= size.x
+			elif hist == 3:
+				if m < 2:
+					szer -= size.x
+				else:
+					gen = _rng.randi_range(0,10)
+					if gen >= 0 and gen <= 6:
+						szer -= size.x
+					elif gen > 6 and gen <= 8:
+						dl += size.y
+					elif gen > 8 and gen <= 10:
+						dl -= size.y
+		szer = size.x
+		dl = size.y
+		hist += 1
+#			if spr == 0:
+#				gen = _rng.randi_range(1,3)
+#			elif spr == 1:
+#				while gen == 1:
+#					gen = _rng.randi_range(0,3)
+#			elif spr == 2:
+#				while gen == 2:
+#					gen = _rng.randi_range(0,3)
+#			elif spr == 3:
+#				gen = _rng.randi_range(0,2)
+#			if gen == 0:
+#				szer += size.x
+#				step_history_x.append(szer)
+#				dl += 0
+#				spr = 0
+#			elif gen == 1:
+#				szer += 0
+#				dl += size.y
+#				step_history_x.append(dl)
+#				spr = 1
+#			elif gen == 2:
+#				szer += -size.x
+#				step_history_x.append(szer)
+#				dl += 0
+#				spr = 2
+#			elif gen == 3:
+#				szer += 0
+#				dl += -size.y
+#				step_history_x.append(dl)
+#				spr = 3
+####
+#		gen = _rng.randi_range(0,1)
+#		if gen == 0:
+#			szer += size.x
+#		else:
+#			dl += size.y
+#		while (szer in step_history_x) or (dl in step_history_y):
+#			gen = _rng.randi_range(0,1)
+#			if gen == 0:
+#				szer -= 2*size.x
+#				dl -= size.y
+#			else:
+#				szer -= size.x
+#				dl -= 2*size.y
+#		step_history_x.append(szer)
+#		step_history_x.append(dl)
+#####
+
+func get_random_tile(probability: float) -> int: #losowanie podłogi lub przeszkody (obecnie nieużywane)
+	return _pick_random_texture(Cell.FLOOR) #if _rng.randf() > probability else _pick_random_texture(Cell.OBSTACLE)
+
+func _pick_random_texture(cell_type: int): #losowanie tekstury z TileMap
+	var interval := Vector2()
+	if cell_type == Cell.WALL:
+		interval = Vector2(4,4)
+	elif cell_type == Cell.FLOOR:
+		interval = Vector2(5,5)
+	elif cell_type == Cell.OBSTACLE:
+		interval = Vector2(0,3)
+	return _rng.randi_range(interval.x, interval.y)
