f69292b38ab45b6cab8e6b74833a3e172f1a40de
Smok7kp <43822731+Smok7kp@users.noreply.github.com>
2021-03-14 23:22:16 +0100

    FlySpear2 (niezbyt działa)
    zczytuje gdzie jest myszka i oblicza od pozycji broni do niej odległość, to się staje rangem; gdzieś tam coś źle oblicza/zaokrągla za dużo i te wartości są z dupy; nwm też czy pozycja myszki nie jest zrypana
    
    debuger działa w flyspear2, jak chciałem w innych broniach zobaczyć jak wygląda attack_vector podczas ataku gra się nie zatrzymuje (nawet w flyspear1)

--- Scenes/Equipment/Weapons/Melee/FlySpear2.gd
+++ Scenes/Equipment/Weapons/Melee/FlySpear2.gd
@@ -3,7 +3,7 @@ extends Node2D
 var mouse_position #Pozycja kursora
 var attack = false #Czy postać atakuje
 var attack_vector = Vector2.ZERO #Wektor po którym porusza się broń podczas ataku
-export var attack_range = 10 #Zasięg ataku
+export var attack_range = 100 #Zasięg ataku
 var timer #Stoper
 var damage
 var a = 1
@@ -11,10 +11,10 @@ var a = 1
 func _physics_process(delta):
 	if a: #Zmienia ustawienia timera i teksturę a także skaluje kolizję (_ready() nie działa)
 		timer.set_wait_time(0.1)
-		$WeaponSprite.texture = load("res://Assets/Loot/Weapons/Spin.png")
+		$WeaponSprite.texture = load("res://Assets/Loot/Weapons/FlySpear2.png")
 		$AttackCollision.scale.x = 1
 		$AttackCollision.scale.y = 1
-		$AttackCollision.position.x = 10
+		$AttackCollision.position.x = 1
 		$AttackCollision.position.y = 1		
 		a = 0
 	if !attack: #Jeżeli nie atakuje to się porusza
@@ -31,21 +31,21 @@ func _physics_process(delta):
 		else:
 			$WeaponSprite.scale.y = 1
 			$WeaponSprite.rotation_degrees=90 #Obróć broń ostrzem do góry
-
 func _on_Player_attacked():
 	if !attack: #Sprawdza czy broń nie jest w trakcie ataku
 		attack = true
 		$AttackCollision.disabled = false
-		var s = 30
-		for i in 2:
-			for n in s:
-				rotation_degrees = n*(360/s)
-				if rotation < -PI/2 or rotation > PI/2:
-					$WeaponSprite.rotation_degrees = -90
-				else:
-					$WeaponSprite.rotation_degrees = -90
-				yield(get_tree().create_timer(0.001), "timeout")
-		$WeaponSprite.rotation_degrees = 0
+		var mouse_position = get_local_mouse_position()
+		var move : float = sqrt(pow((position.x-mouse_position.x),2)+pow((position.y-mouse_position.y),2))
+		var ticks = 30
+		for n in ticks:
+			attack_vector = Vector2((move/ticks) * cos(rotation),(move/ticks) * sin(rotation))
+			position += attack_vector
+			if rotation < -PI/2 or rotation > PI/2:
+				$WeaponSprite.rotation_degrees = -90
+			else:
+				$WeaponSprite.rotation_degrees = 90
+			yield(get_tree().create_timer(0.001), "timeout")
 		$AttackCollision.disabled = true
 		attack = false
 
