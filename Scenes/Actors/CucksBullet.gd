9d0e53acf36e4cecdeadec6d62371499b5d66daf
marycin <bandii819@gmail.com>
2021-03-15 11:12:24 +0100

    sprite changed

---
+++ Scenes/Actors/CucksBullet.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var move = Vector2.ZERO
+var look_vec = Vector2.ZERO #kierunek pocisku
+var player = null 
+var speed = 3 #predkosc pocisku
+var dps = 20 #zadawany damage przez pocisk
+
+func _ready():
+	look_vec = player.position - global_position #kierunek wektora
+	
+func _physics_process(delta):
+	move = Vector2.ZERO
+	self.rotation += PI/6
+	move = move.move_toward(look_vec,delta) #Zmiana położenia o wektor look_vec w czasie delta.
+	move = move.normalized() * speed #Wyrównanie długości wektora nie zależnie od kierunku, ale lepiej sobie zobaczyć na necie
+	position += move	
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"): 
+		body.take_dmg(self) #jezeli pocisk trafi w playera to zadaje dmg o wartosci dps
+	queue_free()
