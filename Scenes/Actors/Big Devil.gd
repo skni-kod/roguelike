2869dc85234979719201c46621744a6768c788d3
Artur Szałyga <arturszalyga@wp.pl>
2021-03-15 01:37:44 +0100

    Dokumentacja i refactor Big Devila, Lil Devila, Fireballa, Lasera oraz Laser Load

--- Scenes/Actors/Big Devil.gd
+++ Scenes/Actors/Big Devil.gd
@@ -1,9 +1,10 @@
 # Big Devil.gd
+
+# LEGENDA
+# BD - Big Devil
+
 extends KinematicBody2D
-# UWAGA WORK IN PROGRESS
-# UWAGA WORK IN PROGRESS
 const LASER_SCENE = preload("Laser.tscn") # wczytuję laser jako LASER_SCENE
-#const LASER_LOAD = preload("Laser_Load.tscn") # wczytuję laser jako LASER_LOAD
 const SPEED = 100 
 
 var player = null
@@ -29,56 +30,56 @@ var target
 var laser_color = Color(1.0, 0, 0, 0.1)
  
 func _ready():
-	health_bar.on_health_updated(health)
+	health_bar.on_health_updated(health) # wczytuję życie
 	$Laser_Load.visible = false
-	$Timer.stop()
 	
 func _physics_process(delta):
 	move = Vector2.ZERO
-	if target != null and int(delta*100) % 5 == 0:
-		aim()
-	if player != null and health>0:
+	if target != null and delta == 0:
+		aim() # strzał w czasie, gdy jakiś target został wyznaczony
+	if player != null and health>0: # gdy BD żyje oraz w jego zasięgu jest gracz
 		$Sprite.scale.x = right
-		move = position.direction_to(player.position) * -speed
-		if player.position.x-self.position.x < 0:
+		move = position.direction_to(player.position) * -speed # odsuwanie się od gracza, gdy jest za blisko
+		if player.position.x-self.position.x < 0: # ustawianie zwrotu sprite w zależności od pozycji gracza wobec BD
 			right = 1
+			$AnimationPlayer.play("Walk") 
 		else:
 			right = -1
-			$AnimationPlayer.play("Walk")
-	elif !attack and health>0:
+			$AnimationPlayer.play("Walk") 
+	elif !attack and health>0: # gdy nie atakuje, a żyje, to wykojune animację Idle
 		$AnimationPlayer.play("Idle")
-	move_and_collide(move)
+	move_and_collide(move) # ruch o Vector2D move
 	
 func _on_Wzrok_body_entered(body):
 	if body != self and body.name == "Player":
-		player = body
+		player = body # player zostaje przypisane jako body, które jest Playerem, gdy wejdzie w Wzrok
 		
 func _on_Wzrok_body_exited(body):
 	if body != self and body.name == "Player":
-		player = null
+		player = null # player zostaje przypisany jako null/nic jak Player opuści Wzrok
 		
 		
 func _on_Atak_body_entered(body):
-	if body != self and body.name == "Player":
+	if body != self and body.name == "Player": # gdy Player wejdzie do sfery Atak
 		target = body
-		attack = true
-		$Laser_Load.visible = true
-		$Timer.start()
+		attack = true # włącza atak
+		$Laser_Load.emit = true # ładowanie lasera zostaje włączone
+		$Timer.start() # włącza Timer
 	
 func _on_Atak_body_exited(body):
-	if body.name == "Player":
-		$Laser_Load.visible = false
-		target = null
-		attack = false
-		$Timer.stop()
+	if body.name == "Player": # gdy Player wyjdzie ze sfery Atak
+		$Laser_Load.emit = false # ładowanie lasera zostaje wyłączone
+		target = null 
+		attack = false # wyłącza atak
+		$Timer.stop() # wyłącza Timer
 
 # celowanie na podstawie bool result, który określa czy ray intersectuje
 # z daną collision_mask + celuje w gracza
 func aim(): 
-	var space_state = get_world_2d().direct_space_state
-	var result = space_state.intersect_ray(position, target.position, [self], collision_mask)
+	var space_state = get_world_2d().direct_space_state # pozyskanie obecnego stanu fizycznego danego World2D w celu wyznaczeń kolizji
+	var result = space_state.intersect_ray(position, target.position, [self], collision_mask) # rzucanie rayem, aby intersectował z danym targetem
 	if result:
-		hit_pos = result.position
+		hit_pos = result.position # pozycja trafienia to pozycja trafienia przez ray targetu
 		if result.collider.name == "Player":
 			$Laser.rotation = (target.position - position).angle() + 2 * Vector2(0.5, -0.5).angle() + 10 * (target.position + target.velocity) # doprecyzowanie rotacji raya
 		
@@ -86,24 +87,24 @@ func aim():
 # strzelanie do target -> pozycja (Vector2) 
 func shoot(target_poz):
 	$AnimationPlayer.play("Attack")
-	var laser = LASER_SCENE.instance()
-	var main = get_tree().get_root().find_node("Main", true, false)
-	laser.position = self.position + $Position2D.position
-	laser.player_Pos = get_tree().get_root().find_node("Player", true, false).position
-	main.add_child(laser)
+	var laser = LASER_SCENE.instance() # załadowanie instancji lasera
+	var main = get_tree().get_root().find_node("Main", true, false) # pozyskanie danego node sceny
+	laser.position = self.position + $Position2D.position # ustawienie pozycji lasera na pozycję Position2D
+	laser.player_Pos = get_tree().get_root().find_node("Player", true, false).position # pozyskanie pozycji gracza
+	main.add_child(laser) # dodanie lasera do sceny
 		
 		
 func _on_Timer_timeout():
 	if health>0 and attack and target: # funkcje gdy żyje
-		shoot(target.position)
-		$Laser_Load.visible = false
-		$Cooldown.start()
+		shoot(target.position) # strzał w stronę pozycji targetu
+		$Laser_Load.emit = false # ładowanie lasera zostaje zatrzymane
+		$Cooldown.start() # timer Cooldown zostaje aktywowany
 		
 		
 func _on_Cooldown_timeout():
-	if health>0 and attack and target:
-		$Laser_Load.visible = true
-		$Timer.start()
+	if health>0 and attack and target: # gdy BD żyje, atakuje i ma cel
+		$Laser_Load.emit = true # Laser Load zostaje emitowany
+		$Timer.start() # Timer zostaje aktywowany w celu oddania strzału
 		
 		
 func get_dmg(dmg):
--- Scenes/Actors/Laser.gd
+++ Scenes/Actors/Laser.gd
@@ -1,77 +1,74 @@
 # Laser.gd
 extends RayCast2D
-# UWAGA WORK IN PROGRESS
-# NIE DZIAŁA TAK JAK POWINNO
-# UWAGA WORK IN PROGRESS
 
-var player_Pos = Vector2.ZERO
-onready var origin = self.position
-var dps = 1
+var player_Pos = Vector2.ZERO # pozycja gracza, pozyskiwana zewnętrznie poprzez przekazanie jego współrzędnych
+onready var origin = self.position # pozycja własna początku lasera
+var dps = 1 # dps lasera
 
-var is_casting := false setget set_is_casting
+var is_casting := false setget set_is_casting # zmienna warunkująca czy laser jest emitowany
+											  # := przypisanie typu do zmiennej, is_casting to bool
+											  # setget set_is_casting - set_is_casting zostaje wywołana moment 
+											  # zanim is_casting zostanie zmieniona, decyduje co zrobić z is_casting
 
 var body = null
 var attack = false
 
 func _ready():
 	set_physics_process(false)
-	$Line2D.points[1] = Vector2.ZERO
-	$Lifetime.start()
-	self.is_casting = true
-	rotation = (player_Pos - origin).normalized().angle()
+	$Line2D.points[1] = Vector2.ZERO # punkt nr 1 $Line2D znajduje się w pozycji Vector2.ZERO
+	$Lifetime.start() # startuje timer z czasem życia lasera
+	self.is_casting = true # laser jest emitowany -> set_is_casting
+	rotation = (player_Pos - origin).normalized().angle() # ustawienie rotacji lasera w stronę gracza
 
 
 func _physics_process(delta):
-	var cast_point := cast_to
-	force_raycast_update()
+	var cast_point := cast_to # przypisanie z typem cast_point to miejsca do którego powinien zostać wyemitowany laser
+	force_raycast_update() # raycast zostaje zupdatowany
 	
-	$CollisionParticles.emitting = is_colliding()
+	$CollisionParticles.emitting = is_colliding() # CollisionParticles zostają emitowane gdy laser na coś natrafi
 	
 	if is_colliding():
-		cast_point = to_local(get_collision_point())
-		$CollisionParticles.global_rotation = get_collision_normal().angle()
-		$CollisionParticles.position = cast_point
+		cast_point = to_local(get_collision_point()) # znajduje punkt kolizji i zamienia go na lokalną pozycję
+		$CollisionParticles.global_rotation = get_collision_normal().angle() # ustawienie globalnej rotacji CollisionParticles
+																			 # wobec kąta normalnego kolizji
+		$CollisionParticles.position = cast_point # pozycja CollisionParticles to punkt, do którego zostają wyemitowane
 		
-		body = self.get_collider()
+		body = self.get_collider() # body to pierwsze ciało z którym skoliduje ray/laser
 		if body.name == "Player":
-			body.take_dmg(get_tree().get_root().find_node("Big Devil", true, false))
+			body.take_dmg(get_tree().get_root().find_node("Laser", true, false)) # body dostaje dps od Lasera
 		
-	$Line2D.points[1] = cast_point
-	$LaserParticles.position = cast_point * 0.5
-	$LaserParticles.process_material.emission_box_extents.x = cast_point.length() * 0.5
+	$Line2D.points[1] = cast_point # punkt nr 1 $Line2D zostaje ustawiony jako cast_point
+	$LaserParticles.position = cast_point * 0.5 # pozycja LaserParticles zostaje ustawiona w połowie długości do cast_point
+	$LaserParticles.process_material.emission_box_extents.x = cast_point.length() * 0.5 # LaserParticles emitują się na całej długości lasera
 
 
 func set_is_casting(cast: bool):
-	is_casting = cast
+	is_casting = cast # is_casting zostaje przypisana zmienna boolowa cast
 	
-	$LaserParticles.emitting = is_casting
-	$CastingParticles.emitting = is_casting
+	$LaserParticles.emitting = is_casting # widoczność LaserParticles zgodnie z is_casting
+	$CastingParticles.emitting = is_casting # widoczność CastingParticles zgodnie z is_casting
 	if is_casting:
-		appear()
+		appear() # funkcja pojawiania się lasera
 	else:
-		$CollisionParticles.emitting = false
-		disappear()
-	set_physics_process(is_casting)
+		$CollisionParticles.emitting = false # CastingParticles off
+		disappear() #funkcja znikania lasera
+	set_physics_process(is_casting) # physics_process zostaje włączony/wyłączony zgodnie z is_casting
 
 
 func appear():
-	$Tween.stop_all()
-	$Tween.interpolate_property($Line2D, "width", 0, 10.0, 0.2)
-	$Tween.start()
-	
+	$Tween.stop_all() # zatrzymuje wszystkie działania Tweena
+	$Tween.interpolate_property($Line2D, "width", 0, 10.0, 0.2) # włącza pojawiania się lasera
+	$Tween.start() # startuje Tweena
+
 
 func disappear():
-	$Tween.stop_all()
-	$Tween.interpolate_property($Line2D, "width", 0, 0.0, 0.2)
-	$Tween.start()
-	yield($Tween, "tween_all_completed")
-	queue_free()
-
-#func _on_Atak_body_entered(body):
-#	if body.name == "Player":
-#		body.take_dmg(self)
+	$Tween.stop_all() # zatrzymuje wszystkie działania Tweena
+	$Tween.interpolate_property($Line2D, "width", 0, 0.0, 0.2)# włącza znikanie lasera
+	$Tween.start() # startuje Tweena
+	yield($Tween, "tween_all_completed") # czeka aż Tween skończy pracę
+	queue_free() # usuwa instancję Lasera
 
 
 func _on_Lifetime_timeout():
-	disappear()
+	disappear() # gdy czas życia minie, laser znika -> disappear()
 
