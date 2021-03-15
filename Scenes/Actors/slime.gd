cb9c318815acd16865505072375d3414c638f7ba
Adrian <dobryemail0@wp.pl>
2021-03-15 11:00:52 +0100

    Dokumentacja skryptu slime'a

--- Scenes/Actors/slime.gd
+++ Scenes/Actors/slime.gd
@@ -1,17 +1,17 @@
 extends KinematicBody2D
 #slime
-var player = null
-var move = Vector2.ZERO
-export var speed = 0.5
-export var dps = 5
-var right = 1
-var attack = false
-var max_hp =61
-var hp:float = max_hp
+var player = null #Zmienna przechowująca węzeł gracza
+var move = Vector2.ZERO #Zmienna inicjująca wektor poruszania
+export var speed = 0.5 #Zmienna przechowująca szybkość poruszania
+export var dps = 5 #Zmienna przechowująca wartość ataku
+var right = 1 #Czy slime jest obrócony w prawo
+var attack = false #Czy slime jest w trakcie ataku
+var max_hp =61 #Zmienna definiująca ilość życia
+var hp:float = max_hp #Zmienna przechowuje ilość pozostałego życia
 
-export var health = 100
-var drop = {"minCoins":0,"maxCoins":5}
-var rng = RandomNumberGenerator.new()
+export var health = 100 #Pozostałe życie w procentach
+var drop = {"minCoins":0,"maxCoins":5} #Przedział definiujący ile slime może zostawić po sobie coinów
+var rng = RandomNumberGenerator.new() #Maszyna Lotto (losuje liczby)
 
 onready var health_bar = $HealthBar
 var floating_dmg = preload("res://Scenes/UI/FloatingDmg.tscn")
@@ -24,13 +24,13 @@ func _ready():
 func _physics_process(delta):
 	move = Vector2.ZERO
 	
-	if player != null and !attack and health>0:
-		$Sprite.scale.x = right
-		move = position.direction_to(player.position) * speed
+	if player != null and !attack and health>0: #Jeżeli gracz jest w polu widzenia i slime nie atakuje oraz życie jest większe niż 0 to
+		$Sprite.scale.x = right #Obróć slime
+		move = position.direction_to(player.position) * speed #Ustaw wektor na pozycję gracza
 		if player.position.x-self.position.x < 0:
-			right = 1
+			right = 1 #Slime ma być obrócony w prawo
 		else:
-			right = -1
+			right = -1 #Slime ma być obrócony w lewo
 		$AnimationPlayer.play("Walk")
 	elif !attack and health>0:
 		$AnimationPlayer.play("Idle")
@@ -38,20 +38,20 @@ func _physics_process(delta):
 	move_and_collide(move)
 
 func _on_Wzrok_body_entered(body):
-	if body != self and body.name == "Player":
+	if body != self and body.name == "Player": #Jeśli gracz wejdzie w pole widzenia to przypisz jego węzeł do zmiennej
 		player = body
 
 func _on_Wzrok_body_exited(body):
-	if body != self and body.name == "Player":
+	if body != self and body.name == "Player": #Jeżeli gracz wyjdzie z pola widzenia to zmienną player ustaw na null
 		player = null
 
 
-func _on_Atak_body_entered(body):
-	if body != self and body.name == "Player":
-		attack = true
+func _on_Atak_body_entered(body): 
+	if body != self and body.name == "Player": #Jeżeli gracz znajdzie się w zasięgu ataku
+		attack = true #Slime atakuje
 
-func _on_Atak_body_exited(body):
-	attack = false
+func _on_Atak_body_exited(body): #Jeżeli gracz wyjdzie z zasięgu ataku
+	attack = false #Slime nie atakuje
 
 func _on_Timer_timeout():
 	if attack and health>0: # funkcje wykonane gdy atakuje
@@ -68,19 +68,17 @@ func get_dmg(dmg):
 	add_child(text)	
 	
 	if health>0:
-		if player != null:
-			if player.position.x-self.position.x < 0:
-				self.position.x += 10
-			else:
-				self.position.x -= 10
+		#Ustal aktualny poziom zdrowia w procentach
 		hp -= dmg
 		health = hp/max_hp*100
 		$AnimationPlayer.play("Hurt")
 		health_bar.on_health_updated(health)
 		health_bar.visible = true
+	#Jeżeli poziom zdrowia spadnie do 0
 	if health<=0:
 		$AnimationPlayer.play("Die")
 		yield($AnimationPlayer,"animation_finished")
+		#Po zakończeniu animacji umierania wyrzuć losową liczbę coinów
 		var level = get_tree().get_root().find_node("Main", true, false)
 		rng.randomize()
 		var coins = rng.randf_range(drop['minCoins'], drop["maxCoins"])
@@ -90,12 +88,7 @@ func get_dmg(dmg):
 			coin = coin.instance()
 			coin.position = randomPosition
 			level.add_child(coin)
-#		var weapon = load("res://Scenes/Loot/Weapon.tscn")
-#		weapon = weapon.instance()
-#		weapon.WeaponName = drop["weapon"]
-#		weapon.position = self.position
-#		level.add_child(weapon)
-		queue_free()
+		queue_free() #Usuń węzeł slime
 		
 	
 		
