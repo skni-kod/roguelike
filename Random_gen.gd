e6c8a3b691abaaa2eeaa6cb4c2acab290ad61b4c
Kacper Maciołek <kacper.maciolek00@gmail.com>
2021-03-17 21:03:10 +0100

    simple generation

--- Random_gen.gd
+++ Random_gen.gd
@@ -1,32 +1,61 @@
 extends Node
 
-var pozycja = [Vector2.ZERO]
-var direction = Vector2.RIGHT
-var rng := RandomNumberGenerator.new() 
-var ilosc_pok = 4
-var kierunki = [Vector2.DOWN,Vector2.UP,Vector2.RIGHT,Vector2.LEFT]
+var directions = [Vector2.DOWN,Vector2.UP,Vector2.RIGHT,Vector2.LEFT]
+var position = Vector2.ZERO
+var map = []
+var rooms = []
+var queue = []
+var rng := RandomNumberGenerator.new()
+var ilosc_pok = 10
+var n = 0
 var szer = 512
 var dl = 288
-var gen
+var gen = 0
 var scene = load("res://Scenes/Levels/Room.tscn")
-func gen():
-	for n in ilosc_pok:
-		if n == 0:
-			var pokoj = scene.instance()
-			add_child(pokoj)
-		else:
-			var pokoj = scene.instance()
-			add_child(pokoj)
-			gen = rng.randi_range(0,3)
-			pozycja.append(Vector2(szer,dl))
-			if gen == 0:
-				pokoj.position.x += szer
-			elif gen == 1:
-				pokoj.position.x -= szer
-			if gen == 2:
-				pokoj.position.y += dl
-			elif gen == 3:
-				pokoj.position.y -= dl
-				
+
+func generate():
+	rng.randomize()
+	var pokoj = scene.instance()
+	add_child(pokoj)
+	map.append(pokoj.position)
+	gen = 1
+	while n < ilosc_pok - 1:
+		rng.randomize()
+		randomize()
+		if n != 0:
+			gen = rng.randi_range(1,2)
+		directions.shuffle()
+		for i in range(gen):
+			step(directions[i])
+			n += 1
+			if n >= ilosc_pok - 1:
+				break
+		if queue:
+			position = queue.pop_front()
+
+func step(direction):
+	var target_position = position + direction
+	if not target_position in map:
+		var pokoj = scene.instance()
+		add_child(pokoj)
+		pokoj.position.x = target_position.x * szer
+		pokoj.position.y = target_position.y * dl
+		rooms.append(pokoj)
+		map.append(target_position)
+		queue.append(target_position)
+	else:
+		n -= 1
+
 func _ready():
-	gen()
+	generate()
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		for room in rooms:
+			room.queue_free()
+		map = []
+		rooms = []
+		queue = []
+		position = Vector2.ZERO
+		n = 0
+		generate()
