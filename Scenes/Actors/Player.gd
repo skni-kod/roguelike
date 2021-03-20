2a4c15052477c289ddabdc5ff55c60839d880679
marycin <bandii819@gmail.com>
2021-03-20 13:52:07 +0100

    UI not ended
    Podnoszenie broni z ziemi działa, raz na jakis czas sie wykrzaczy (jeszcze niewiem dlaczego), zmiana broni pod przyciskiem Q dziala, jest problem z damage broni, ktora jest w "kieszeni"

--- Scenes/Actors/Player.gd
+++ Scenes/Actors/Player.gd
@@ -36,55 +36,90 @@ func _ready(): #po inicjacji bohatera
 		"Axe" : preload("res://Assets/Loot/Weapons/axe.png"),
 		"Blade" : preload("res://Assets/Loot/Weapons/blade.png"),
 		"BloodSword" : preload("res://Assets/Loot/Weapons/BloodSword.png"),
-		"FireScepter" : preload("res://Assets/Loot/Weapons/firescepter.png"),
+		"Fire Scepter" : preload("res://Assets/Loot/Weapons/firescepter.png"),
 		"FMS" : preload("res://Assets/Loot/Weapons/FMS.png"),
-		"Hammer" : preload("res://Assets/Loot/Weapons/Hammer.png"),
-		"Katana" : preload("res://Assets/Loot/Weapons/Katana.png"),
-		"Knife" : preload("res://Assets/Loot/Weapons/Knife.png"),
-		"Spear" : preload("res://Assets/Loot/Weapons/Spear.png")
+		"Hammer" : preload("res://Assets/Loot/Weapons/hammer.png"),
+		"Katana" : preload("res://Assets/Loot/Weapons/katana.png"),
+		"Knife" : preload("res://Assets/Loot/Weapons/knife.png"),
+		"Spear" : preload("res://Assets/Loot/Weapons/spear.png")
 	}
 	weapons = {
-		"Slot1" : "Blade",
-		"Slot2" : "Empty"
+		1 : "Blade",
+		2 : "Empty"
 	}
-	ui_access_wslot1.texture = all_weapons[weapons["Slot1"]]
-
+	ui_access_wslot1.texture = all_weapons[weapons[1]]
+	
 func _physics_process(delta): #funkcja wywoływana co klatkę
-
+	if weaponToTake == null:
+		print("lalalallala")
+	else:
+		print("huehuehue")
 	if Input.is_action_just_pressed("attack"): #jeżeli przycisk "attack" został wsciśnięty
 		emit_signal("attacked") #wyemituj sygnał że bohater zaatakował
 	else: #Jeżeli nie atakuje to
 		movement() #wywołanie funkcji poruszania się
 	if weaponToTake != null: #Jeżeli gracz stoi przy broni do podniesienia
 		if Input.is_action_just_pressed("pick"): #Jeżeli nacisnął przycisk podniesienia
-			var weaponName = weaponToTake.WeaponName
-			if equipped != weaponName:
-				if weapons["Slot2"] == "Empty":
-					weapons["Slot2"] = weaponName
-					ui_access_wslot2.texture = all_weapons[weaponName]
+			if equipped != weaponToTake.WeaponName:
+				print("gitens")
+				if weapons[2] == "Empty":
+					swap_weapon(2,weaponToTake)
 				else:
-					var current_weapon = check_current_weapon()
-					if current_weapon == 1:
-						weapons["Slot1"] = all_weapons[weaponName]
-						ui_access_wslot1.texture = all_weapons[weaponName]
-						actualweapon_access.texture = all_weapons[weaponName]
-					elif current_weapon == 2:
-						weapons["Slot2"] = weaponName
-						ui_access_wslot2.texture = all_weapons[weaponName]
-						actualweapon_access.texture = all_weapons[weaponName]
-						
+					swap_weapon(check_current_weapon(),weaponToTake)
 	if chest != null: #Jeżeli gracz stoi przy skrzyni
 		if Input.is_action_just_pressed("pick"):
 			emit_signal("open") #Wyślij sygnał otwórz
 			chest = null
+	if weapons[2] != "Empty":
+		if Input.is_action_just_pressed("change_weapon_slot"):
+			change_weapon_slot(check_current_weapon())
 			
 func check_current_weapon():
-	var current_weapon = actualweapon_access.texture
-	if all_weapons[weapons["Slot1"]] == actualweapon_access.texture:
+	if all_weapons[weapons[1]] == actualweapon_access.texture:
 		return 1
-	elif all_weapons[weapons["Slot2"]] == actualweapon_access.texture:
+	elif all_weapons[weapons[2]] == actualweapon_access.texture:
 		return 2
 
+func change_weapon_slot(currentSlot):
+	if currentSlot == 1:
+		$EquippedWeapon.position=Vector2.ZERO
+		$EquippedWeapon.set_script(load('res://Scenes/Equipment/Weapons/Melee/'+str(weapons[2])+'.gd')) #Tylko melee poki co ;/
+		$EquippedWeapon.timer = $EquippedWeapon/Timer
+		#$EquippedWeapon.damage = int(weaponOnGround.Stats['attack']) to trzeba jakos inaczej
+	else:
+		$EquippedWeapon.position=Vector2.ZERO
+		$EquippedWeapon.set_script(load('res://Scenes/Equipment/Weapons/Melee/'+str(weapons[1])+'.gd'))
+		$EquippedWeapon.timer = $EquippedWeapon/Timer
+		#$EquippedWeapon.damage = int(weaponOnGround.Stats['attack']) to trzeba jakos inaczej
+
+func swap_weapon(slot,weaponOnGround):
+	if weapons[2] != "Empty":
+		if slot == 1:
+			ui_access_wslot1.texture = all_weapons[weaponOnGround.WeaponName]
+		elif slot == 2:
+			ui_access_wslot2.texture = all_weapons[weaponOnGround.WeaponName]
+		var weaponUsed = load("res://Scenes/Loot/Weapon.tscn")
+		weaponUsed = weaponUsed.instance()
+		weaponUsed.WeaponName = str(weapons[slot])
+		weaponUsed.position = weaponOnGround.position
+		level.add_child(weaponUsed)
+		weapons[slot] = weaponOnGround.WeaponName
+		$EquippedWeapon.position=Vector2.ZERO
+		$EquippedWeapon.set_script(load('res://Scenes/Equipment/Weapons/'+weaponOnGround.Stats['range']+'/'+weaponOnGround.WeaponName+'.gd'))
+		$EquippedWeapon.timer = $EquippedWeapon/Timer
+		$EquippedWeapon.damage = float(weaponOnGround.Stats['attack'])
+	else:
+		ui_access_wslot2.texture = all_weapons[weaponOnGround.WeaponName]
+		weapons[2] = weaponOnGround.WeaponName
+		$EquippedWeapon.position=Vector2.ZERO
+		$EquippedWeapon.set_script(load('res://Scenes/Equipment/Weapons/'+weaponOnGround.Stats['range']+'/'+weaponOnGround.WeaponName+'.gd'))
+		$EquippedWeapon.timer = $EquippedWeapon/Timer
+		$EquippedWeapon.damage = float(weaponOnGround.Stats['attack'])
+	weaponOnGround.queue_free()
+	weaponOnGround=null
+	
+	
+		
 func movement(): #funkcja poruszania się
 	direction = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
@@ -122,6 +157,7 @@ func _on_Pick_body_entered(body): #Jeśli coś do podniesienia jest w zasięgu g
 			coins += 10
 			body.queue_free()
 		elif "Weapon" in body.name:
+			print("Brońka")
 			weaponToTake = body
 		elif "Chest" in body.name:
 			chest = body
@@ -132,5 +168,6 @@ func _on_Player_health_updated(health): #pusta funkcja która pozwala na poprawn
 
 func _on_Pick_body_exited(body): #Rozwiązanie tymczasowe
 	weaponToTake = null
+	print("NieBrońka")
 	chest = null
 
