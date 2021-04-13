33cb824bbf8126c88fd3f31976d025ae81dfbcf0
Mixu285 <remik285@gmail.com>
2021-04-13 16:14:47 +0200

    Poprawa
    Komentarze i przeniesienie folderów

--- Scenes/Levels/Random_gen.gd
+++ Scenes/Levels/Random_gen.gd
@@ -4,49 +4,49 @@ signal boss(bossRoom)
 
 var directions = [Vector2.DOWN,Vector2.UP,Vector2.RIGHT,Vector2.LEFT]
 var position = Vector2.ZERO
-var map = []
-var rooms = []
-var queue = []
+var map = [] #lista zawierająca współrzedne pokojów
+var rooms = [] #lista pokojów
+var queue = [] #kolejka do przechowywania koordynatów do generowania pokojów
 var rng := RandomNumberGenerator.new()
-var roomsNum = 20
-var n = 0
-var szer = 512
-var dl = 288
+var roomsNum = 20 #ilość pokojów
+var n = 0 #zmiennna pomocnicza do funkcji generate
+var szer = 512 #szerokość pokoju
+var dl = 288 #długość pokoju
 var gen = 0
 var oneDoor = 0
 var drawn = false
-var scene = load("res://Scenes/Levels/Room.tscn")
-var player = load("res://Scenes/Actors/Player.tscn")
+var scene = load("res://Scenes/Levels/Room.tscn") #wczytywanie sceny pokoju
+var player = load("res://Scenes/Actors/Player.tscn") #wczytywanie sceny playera
 
-func draw(map):
-	var oneDoorRooms = []
-	var furthestRoom = [Vector2.ZERO]
-	for i in range(len(map)):
+func draw(map): #rysowanie poziomu na podstawie wygenerowanych koordynatów pokojów
+	var oneDoorRooms = [] #lista pokojów z jednymi otwartymi drzwiami
+	var furthestRoom = [Vector2.ZERO] #najdalszy pokój
+	for i in range(len(map)): #respienie pokojów
 		var doors = 0
 		var room = scene.instance()
-		add_child(room)
+		add_child(room) #dodawanie sceny pokoju
 		var tilemap = room.get_node("TileMap")
-		room.position.x = map[i].x * szer
-		room.position.y = map[i].y * dl
-		if not map[i] + Vector2.DOWN in map:
+		room.position.x = map[i].x * szer #przypisywanie pozycji x pokoju 
+		room.position.y = map[i].y * dl #przypisywanie pozycji y pokoju
+		if not map[i] + Vector2.DOWN in map: #jeżeli nie ma pokoju pod pokojem, zamknij drzwi
 			tilemap.set_cell(6,8,28)
 			tilemap.set_cell(7,8,29)
 			tilemap.set_cell(8,8,30)
 			tilemap.set_cell(7,7,31)
 			doors += 1
-		if not map[i] + Vector2.UP in map:
+		if not map[i] + Vector2.UP in map: #jeżeli nie ma pokoju nad pokojem, zamknij drzwi
 			tilemap.set_cell(6,0,24)
 			tilemap.set_cell(7,0,25)
 			tilemap.set_cell(8,0,26)
 			tilemap.set_cell(7,1,27)
 			doors += 1
-		if not map[i] + Vector2.RIGHT in map:
+		if not map[i] + Vector2.RIGHT in map: #jeżeli nie ma pokoju na prawo od pokoju, zamknij drzwi
 			tilemap.set_cell(14,3,20)
 			tilemap.set_cell(14,4,21)
 			tilemap.set_cell(14,5,22)
 			tilemap.set_cell(13,4,23)
 			doors += 1
-		if not map[i] + Vector2.LEFT in map:
+		if not map[i] + Vector2.LEFT in map: #jeżeli nie ma pokoju na lewo od pokoju, zamknij drzwi
 			tilemap.set_cell(0,3,16)
 			tilemap.set_cell(0,4,17)
 			tilemap.set_cell(0,5,18)
@@ -54,13 +54,13 @@ func draw(map):
 			doors += 1
 		rooms.append(room)
 		if doors == 3:
-			oneDoorRooms.append([map[i], room])
-	for room in oneDoorRooms:
+			oneDoorRooms.append([map[i], room]) #dodawanie do listy pokoju z tylko jednymi drzwiami
+	for room in oneDoorRooms: #szukanie najdalszego pokoju
 		if abs(room[0].length()) > abs(furthestRoom[0].length()):
 			furthestRoom = room
 	emit_signal("boss", furthestRoom[0])
 
-func generate():
+func generate(): #funkcja generująca poziom (położenia pokojów)
 	map.append(Vector2(0,0))
 	rng.randomize()
 	gen = rng.randi_range(1,4)
@@ -82,7 +82,7 @@ func generate():
 			n = 0
 			generate()
 	oneDoor = 0
-	for room in map:
+	for room in map: #określanie ile jest zamkniętych drzwi w pokoju
 		if not room == Vector2.ZERO:
 			var doors = 0
 			if not room + Vector2.DOWN in map:
@@ -106,7 +106,7 @@ func generate():
 		oneDoor = 0
 		generate()
 
-func step(direction):
+func step(direction): #funkcja, która określa w którą stronę może zostać wygenerowany pokój
 	var target_position = position + direction
 	if not target_position in map:
 		map.append(target_position)
