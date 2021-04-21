77bfb6735ad5d82ba36abae23786ce9684956f51
Smok7kp <43822731+Smok7kp@users.noreply.github.com>
2021-04-21 18:25:55 +0200

    Bug naprawiony
    sprawdzanie czy skończyły się potki na danym slocie jest wyżej żeby dalej już ten slot był czytany jako "empty";
    puźniej akutalizuje liczbe i obrazek tylko jak oba albo jeden sloty są zapełnione, jeśli nie ma aktualizacji danego slotu to numerek i tekstura znika, dzięki czemu nie zostaje zero potek tylko pusty slot

--- Scenes/Actors/Player.gd
+++ Scenes/Actors/Player.gd
@@ -49,25 +49,25 @@ var base_hp = null
 var Potion_in_time = 0
 
 func UpdatePotions(): #funkcja aktualizująca status potek
-	if potions[2] == "Empty": #jeżeli niema potki na slocie drugim to:
-		ui_access_pslot1.texture = all_potions[potions[1]] #przypisanie do textury slotu pierwszego textury aktualnego pierwszego potka
-		potion1_amount.text = str(potions_amount[potions[1]]) #aktualizacja textu ilości potek w eq
-		ui_access_pslot2.texture = null # usuniecie tekstury z slotu drugiego
-		potion2_amount.text = "" # ustawienie textu ilosci potek na nic
-	else:
+	if potions_amount[potions[1]] == 0: #jeżeli ilosc potek na slocie 1 jest rowna 0 to:
+		ui_access_pslot1.texture = null  # usuniecie tekstury z slotu pierwszego
+		potion1_amount.text = "" # ustawienie textu ilosci potek na nic
+		potions[1] = "Empty" #przypisanie potxe z slotu 1 nazwe Empty potrzebne do poprawnego działania
+	if potions_amount[potions[2]] == 0: #to samo co wyżej tylko dla slotu 2
+		ui_access_pslot2.texture = null
+		potion2_amount.text = ""
+		potions[2] = "Empty"
+		
+	if potions[2] != "Empty" and potions[1] != "Empty": #jeżeli niema potki na slocie pierwszy ani drugim to:
 		ui_access_pslot1.texture = all_potions[potions[1]] #przypisanie do textury slotu pierwszego textury aktualnego pierwszego potka
 		ui_access_pslot2.texture = all_potions[potions[2]] #przypisanie do textury slotu drugiego textury aktualnego drugiego potka
 		potion1_amount.text = str(potions_amount[potions[1]]) #aktualizacja textu ilości potek w eq
 		potion2_amount.text = str(potions_amount[potions[2]]) #aktualizacja textu ilości potek w eq
+	elif potions[1] != "Empty":
+		ui_access_pslot1.texture = all_potions[potions[1]] #przypisanie do textury slotu pierwszego textury aktualnego pierwszego potka
+		potion1_amount.text = str(potions_amount[potions[1]]) #aktualizacja textu ilości potek w eq
 		
-		if potions_amount[potions[1]] == 0: #jeżeli ilosc potek na slocie 1 jest rowna 0 to:
-			ui_access_pslot1.texture = null  # usuniecie tekstury z slotu pierwszego
-			potion1_amount.text = "" # ustawienie textu ilosci potek na nic
-			potions[1] = "Empty" #przypisanie potxe z slotu 1 nazwe Empty potrzebne do poprawnego działania
-		if potions_amount[potions[2]] == 0: #to samo co wyżej tylko dla slotu 2
-			ui_access_pslot2.texture = null
-			potion2_amount.text = ""
-			potions[2] = "Empty"
+	
 
 
 func _ready(): #po inicjacji bohatera
