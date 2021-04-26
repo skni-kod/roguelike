fe0b50ad63a949affea27706ae9ef0aff08d4a9e
Grzegorz Zięba <79113140+Finito21@users.noreply.github.com>
2021-04-26 21:30:30 +0200

    Knight
    narazie zrobiona klasa rycerz

---
+++ ClassStats.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name ClassStats
+
+export(String) var type="Knight"
+export(int) var health=100
+export(int) var strength=100
+export(int) var intelligence=100
+export(Image) var profile = null
