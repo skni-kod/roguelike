4a689794cee08edac18256b780689383012eeda1
Grzegorz Zięba <79113140+Finito21@users.noreply.github.com>
2021-05-04 22:59:49 +0200

    3 klasy do wybory
    zostało to połączyć i skleic w 1

--- Scenes/class_select/ClassDisplay.gd
+++ Scenes/class_select/ClassDisplay.gd
@@ -1,1 +1,17 @@
 extends ColorRect
+
+onready var profile_image=get_node("MarginContainer/HBoxContainer/CenterContainer/TextureRect")
+onready var class_label=get_node("MarginContainer/HBoxContainer/VBoxContainer/Class/Data")
+onready var health_label=get_node("MarginContainer/HBoxContainer/VBoxContainer/Health/Data")
+onready var strength_label=get_node("MarginContainer/HBoxContainer/VBoxContainer/Strength/Data")
+onready var intelligence_label=get_node("MarginContainer/HBoxContainer/VBoxContainer/Intelligence/Data")
+func _ready():
+	var class_data = load("res://Scenes/class_select/Knight.tres")
+	update_class_display(class_data)
+	
+func update_class_display(class_data):
+	profile_image.texture=class_data.profile
+	class_label.text=class_data.type
+	health_label.text=str(class_data.health)
+	strength_label.text=str(class_data.strength)
+	intelligence_label.text=str(class_data.intelligence)
