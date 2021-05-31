9eb55186e799572088f66fc031c4100977988833
Adrian <dobryemail0@wp.pl>
2021-05-31 15:28:56 +0200

    Knife first ability

--- Scenes/Equipment/Weapons/Melee/Knife.gd
+++ Scenes/Equipment/Weapons/Melee/Knife.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var player_node = get_tree().get_root().find_node("Player", true, false)
+
 var mouse_position #Pozycja kursora
 var attack = false #Czy postać atakuje
 var attack_vector = Vector2.ZERO #Wektor po którym porusza się broń podczas ataku
@@ -8,6 +10,7 @@ var timer #Stoper
 var damage
 var weaponKnockback
 var a = 1
+var skill = 0
 
 var smoothing = 1
 
@@ -44,7 +47,24 @@ func _physics_process(delta):
 
 
 	if Input.is_action_just_pressed("use_ability_1"):
-		var placeholder5 #kilkanie przycisku działa abilitki trza zrobić
+		if player_node.mana > 25 and !skill:
+			skill = 1
+			player_node.mana -= 25
+			for i in range(0,5):
+				
+				if !attack and attack_speed==0:
+					attack = true
+				if i%2 == 0:rotation += .2
+				else:rotation -= .2*2
+				attack_vector = Vector2(attack_range * cos(rotation), attack_range * sin(rotation))
+				if rotation < -PI/2 or rotation > PI/2:
+					$WeaponSprite.rotation_degrees = -90#-90
+				else:
+					$WeaponSprite.rotation_degrees = 90#90
+				$AttackCollision.disabled = false
+				yield(get_tree().create_timer(.1), "timeout")
+				timer.start()
+			skill = 0
 	if Input.is_action_just_pressed("use_ability_2"):
 		var palceholder20
 		
@@ -74,7 +94,8 @@ func _on_Timer_timeout(): #Wykonuje się kiedy zejdzie cooldown ataku
 		position -= attack_vector*(animation_step/swing_back)
 	else:
 		$AttackCollision.disabled = true
-		attack = false
+		if !skill:
+			attack = false
 		attack_speed=0		
 		timer.stop()
 		reset_pivot()
