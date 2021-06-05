fda781a836fbe8ad24d53e333e6ae34950972bd9
Smok7kp <43822731+Smok7kp@users.noreply.github.com>
2021-06-06 00:13:43 +0200

    Skończone
    Passive: po każdym ataku można wykonać doskok
    A1: Obraca wokoło siebie włucznią i odpycha wrogów
    A2: Seria nieprecyzyjnych ataków w stożku

--- Scenes/Equipment/Weapons/Melee/Spear.gd
+++ Scenes/Equipment/Weapons/Melee/Spear.gd
@@ -8,9 +8,9 @@ var attack_vector = Vector2.ZERO
 export var attack_range = 15
 var timer #Cooldown pomiędzy atakami
 var damage #Obrażenia zadawane przez broń. Wartość pobierana z pliku
-var ability1ManaCost=1
-var ability2ManaCost=1
-var SR=0 #Stab rotation, potrzebne do ability 1
+var ability1ManaCost=1 #koszt do zmiany w balansie
+var ability2ManaCost=1 #koszt do zmiany w balansie
+var SR=0 #Stab rotation, potrzebne do ability 2
 var weaponKnockback
 var isWeaponReady=1 #Sprawdź czy broń jest gotowa do ataku
 var smoothing = 1
@@ -65,13 +65,6 @@ func _physics_process(delta):
 		else:
 			print("Insufficient mana, " + String(ability2ManaCost) +" required to cast ability")
 
-
-	if Input.is_action_just_pressed("use_ability_1"):
-		var placeholder5 #kilkanie przycisku działa abilitki trza zrobić
-	if Input.is_action_just_pressed("use_ability_2"):
-		var palceholder20
-		
-	
 func reset_pivot():#Zresetuj broń. Nawet jak animacja jest spieprzona to broń nie oddali się od gracza
 	position.x=0.281
 	position.y=0.281
@@ -101,7 +94,14 @@ func _on_Timer_timeout():
 		attack_speed=0		
 		timer.stop()
 		reset_pivot()
-		
+		##########PASSIVE####### po każdym ataku można wykonać doskok
+		player_node.skok_vector=Vector2.ZERO #sprawia że nie skaczesz jak się nie poruszasz
+		player_node.speed=70 #zmniejsza prędkość żeby skok był krótszy od zwykłego
+		player_node.jump() #skok ten sam co normalny
+		yield(get_tree().create_timer(0.5), "timeout") #czas po którym speed gracza ma wrócić do normalnej wartości
+		player_node.speed=100 #wracamy do bazowej prędkości gracza
+		player_node.skok_vector=player_node.direction #wracamy do normalnej wartości by gracz mógł skakać stojąc w miejscu
+		########################
 func change_weapon(texture):
 	$WeaponSprite.texture = texture
 
@@ -109,24 +109,42 @@ func _on_EquippedWeapon_body_entered(body):
 	if body.is_in_group("Enemy"):
 		body.get_dmg(damage, weaponKnockback)
 
-func ability1():
-	swing_to = 0.1
-	paused = 0.1
-	swing_back = 0.1
+func ability1(): #obraca wokoło siebie włucznią i odpycha przeciników
 	
-	var ticks = 30
+	attack = true
+	weaponKnockback += 3 #zwiększamy odżut
+	$AttackCollision.scale.x = 3 #zwiększamy hitboxy żeby były tak długie jak włucznia przy pełym oddaleniu
+	$AttackCollision.disabled = false
+	var s = 30 #ilość kroków w obrocie
+	for i in 4: #ilość obrotów
+		for n in s:
+			rotation_degrees = n*(360/s) #ustawiamy pozycję co odpowiedni krok
+			$WeaponSprite.rotation_degrees = n*(360/s) #sprawia że broń robi SPINNNNNNNNN
+			yield(get_tree().create_timer(0.001), "timeout") #czas pomiedzy krokiem żeby wszystko nie stało się w sekundę
+	weaponKnockback -= 3 #wracamy do bazowego odżutu
+	$AttackCollision.scale.x = 1.1 #wracamy do bazowej kolizji
+	$AttackCollision.disabled = true
+	attack = false
+	reset_pivot()
+
+func ability2(): #seria szybkich nieprecyzyjnych ataków w stożku
+	
+	swing_to = 0.1 #zmieniamy zmienne od ataku żeby były szybsze
+	paused = 0.1 
+	swing_back = 0.1 
+	
+	var ticks = 30 #ilość ataków
 	for n in ticks:
-		var rng = RandomNumberGenerator.new() ##
-		rng.randomize() ##
-		SR = rng.randi_range(-10, 10) ##
-		_on_Player_attacked()
-		yield(get_tree().create_timer(0.1), "timeout")
+		var rng = RandomNumberGenerator.new() #generujemy randomowy numer
+		rng.randomize() 
+		SR = rng.randi_range(-10, 10) #szerokość stożka
+		_on_Player_attacked() #używamy zwykłego ataku
+		yield(get_tree().create_timer(0.1), "timeout") #przerwy bo wszystko by się stało na raz
 	
-	swing_to = 0.3
+	swing_to = 0.3 #wracamy do bazowych zmiennych ataku
 	paused = 0.4 
 	swing_back = 0.5
-	SR=0
+	SR=0 #resetujemy stab rotation bo atak by został na zawsze z rotacja ostatniego z serii
 
 
-func ability2():
-	print("xd")
+
