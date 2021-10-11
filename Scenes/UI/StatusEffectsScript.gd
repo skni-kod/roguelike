6889886af1bedd126c2990494d503430c045e74d
Artur Szałyga <arturszalyga@wp.pl>
2021-10-11 16:06:40 +0200

    StatusNode added

---
+++ Scenes/UI/StatusEffectsScript.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+signal burn(giver, reciever, lvl, prob, time)
+signal bleed(giver, reciever, lvl, prob, time)
+signal poison(giver, reciever, lvl, prob, time)
+
+signal freeze(giver, reciever, lvl, prob, time)
+signal knockback(giver, reciever, lvl, prob, time) 
+signal weakness(giver, reciever, lvl, prob, time)
+signal healing(giver, reciever, lvl, prob, time)
