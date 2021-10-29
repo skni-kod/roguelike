cae334944a8e0696b18bfa5f741e995f183b134b
DominikaMa3ur <169584@stud.prz.edu.pl>
2021-10-29 22:18:04 +0200

    Ośmiornica v0.1
    + podstawowa grafika
    + podstawowe animacje
    + podstawowy skrypt
    
    Do dodania/poprawy: grafika, animacje, skrypt, mapowanie normalnych, pasek punktów życia, kolizja gracza z ośmiornicą/mackami

--- Scenes/Actors/OctoBoss/OctoBoss.gd
+++ Scenes/Actors/OctoBoss/OctoBoss.gd
@@ -1,6 +1,17 @@
 extends StaticBody2D
 
 var macka = preload("res://Scenes/Actors/OctoBoss/tentacle.tscn")
+var gracz
+var pozycjaMacki
 
 func _ready():
-	pass
+	gracz = get_parent().get_node("Player")
+
+func _on_Timer_timeout():
+	$AnimationPlayer.play("mrug")
+	pozycjaMacki = gracz.position
+
+func _on_Timer2_timeout():
+	var m = macka.instance()
+	m.position = pozycjaMacki
+	get_parent().add_child(m)
