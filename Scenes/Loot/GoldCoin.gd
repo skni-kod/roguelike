51b77fdeb545318c924b5927755e35e2fbb102bb
Maksymilian Dendura <maksd0410@gmail.com>
2021-10-31 20:10:17 +0100

    Pierwsza wersja animacji

--- Scenes/Loot/GoldCoin.gd
+++ Scenes/Loot/GoldCoin.gd
@@ -1,4 +1,13 @@
 extends StaticBody2D
 
+var rng = RandomNumberGenerator.new()
+
 func _ready():
 	$AnimationPlayer.play("Idle") #Odtwarzaj animację obracania
+	#Wylosowanie liczby i odtworzenie animacji wyapadnia w lewo lub prawo
+	rng.randomize()
+	var random_number = rng.randi_range(0, 1)
+	if (random_number == 0):
+		$AnimationPlayer2.play("Drop-left")
+	else:
+		$AnimationPlayer2.play("Drop-right") 
