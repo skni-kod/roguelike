b8f349c4b358819b679bb1c63ccea60655bab318
DominikaMa3ur <169584@stud.prz.edu.pl>
2021-10-31 22:14:06 +0100

    Ośmiornica v0.2
    Dodane/poprawione:
    + punkty życia
    + kolizja z graczem
    + zadawanie obrażeń graczowi
    Anulowane:
    - mapowanie normalnych
    
    Do dodania/poprawy: atakowanie gracza (animacja), chowanie macek (animacja), tekstura paska życia, algorytm generacji macek

--- Scenes/Actors/OctoBoss/OctoBoss.gd
+++ Scenes/Actors/OctoBoss/OctoBoss.gd
@@ -1,17 +1,37 @@
 extends StaticBody2D
 
+export var max_hp = 900
+var hp = max_hp
 var macka = preload("res://Scenes/Actors/OctoBoss/tentacle.tscn")
+var pasek_hp
 var gracz
 var pozycjaMacki
+var Timer2 = false
 
 func _ready():
+	pasek_hp = preload("res://Scenes/UI/BossHealthBar.tscn").instance()
+	get_node("../UI").add_child(pasek_hp)
 	gracz = get_parent().get_node("Player")
 
 func _on_Timer_timeout():
-	$AnimationPlayer.play("mrug")
-	pozycjaMacki = gracz.position
+	$AnimationPlayer.play("mrug") #odtwarza animację mrugnięcia
+	pozycjaMacki = gracz.position #pobiera pozycję gracza
 
-func _on_Timer2_timeout():
+func _on_Timer2_timeout(): #0.2s po poprzednim
+	if (!Timer2): 
+		#za pierwszym razem licznik czasu jest ustawiony na 5.2 s,
+		#potem na 5 s, aby utrzymywać stały odstęp czasu
+		Timer2 = true
+		$Timer2.wait_time = 5
 	var m = macka.instance()
 	m.position = pozycjaMacki
 	get_parent().add_child(m)
+
+func dmg(dmg): #przyjmowanie obrażeń od ciosów w macki
+	hp -= dmg
+	pasek_hp.value = hp/max_hp * 100
+	if hp <= 0:
+		die()
+
+func die():
+	self.queue_free()
--- Scenes/Actors/OctoBoss/tentacle.gd
+++ Scenes/Actors/OctoBoss/tentacle.gd
@@ -1,4 +1,27 @@
 extends KinematicBody2D
 
+var gracz
+var u = 10 #macki nie posiadają określonej ilości HP
+#po przyjęciu u ciosów chowają się
+
 func _ready():
 	$AnimationPlayer.play("idle")
+
+func get_dmg(dmg, weaponKnockback):
+	get_parent().get_node("OctoBoss").dmg(dmg) #obrażenia są odejmowane od zdrowia głównego bossa
+	u -= 1
+	if (u <= 0):
+		self.queue_free() #po u ciosach macka znika
+
+
+func _on_Zasieg_body_entered(body):
+	if body.name == "Player":
+		gracz = body
+
+func _on_Zasieg_body_exited(body):
+	if body.name == "Player":
+		gracz = null
+
+func _on_Timer_timeout():
+	if gracz:
+		gracz.take_dmg(20, 0, 0)
