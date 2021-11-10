5c4df4759cfccf11162ef2b073b3d3dad2c84335
DominikaMa3ur <169584@stud.prz.edu.pl>
2021-11-10 22:49:04 +0100

    Ośmiornica BETA
    + dodano animacje
    + zmieniono i dopracowano mechanikę: macki nie atakują już na odległość i chowają się kilku sekundach, ale pojawiają się znacznie częściej i atakują wypychając gracza w górę
    + dodano upuszczanie monet przez ośmiornicę i zwiększono ilość monet upuszczanych przez PandoBossa
    + częściowo naprawiono problem blokowania się gracza w środku macki
    
    Do dodania: indywidualna tekstura paska życia, dopracować animacje(?), okomentować kod, uniemożliwić blokowanie gracza w środku macki

--- Scenes/Actors/OctoBoss/OctoBoss.gd
+++ Scenes/Actors/OctoBoss/OctoBoss.gd
@@ -7,7 +7,11 @@ var macka = preload("res://Scenes/Actors/OctoBoss/tentacle.tscn")
 var pasek_hp
 var gracz
 var pozycjaMacki
+var swap = null
 var Timer2 = false
+var drop = {"minCoins":30,"maxCoins":60} # zakres minimalnej i maksymalnej ilości pieniędzy
+var randomPosition # zmienna losowej pozycji dla coinsów
+var rng = RandomNumberGenerator.new() # zmienna generująca nowy generator losowej liczby
 
 func _ready():
 	pasek_hp = preload("res://Scenes/UI/BossHealthBar.tscn").instance()
@@ -21,13 +25,22 @@ func _on_Timer_timeout():
 func _on_Timer2_timeout(): #0.2s po poprzednim
 	if gracz:
 		if (!Timer2): 
-			#za pierwszym razem licznik czasu jest ustawiony na 5.2 s,
-			#potem na 5 s, aby utrzymywać stały odstęp czasu
+			#za pierwszym razem licznik czasu jest ustawiony na 1.1 s,
+			#potem na 0.9 s, aby utrzymywać stały odstęp czasu
 			Timer2 = true
-			$Timer2.wait_time = 5
+			$Timer2.wait_time = 0.9
 		var m = macka.instance()
-		add_child(m)
-		m.global_position = pozycjaMacki
+		if pozycjaMacki == gracz.global_position:
+			gracz.take_dmg(10, 1.1, Vector2(gracz.global_position.x, gracz.global_position.y + 10))
+			$miniTimer.start()
+			swap = m
+		elif sqrt((pozycjaMacki.x - gracz.global_position.x)*(pozycjaMacki.x - gracz.global_position.x) + (pozycjaMacki.y-gracz.global_position.y)*(pozycjaMacki.y-gracz.global_position.y)) < 30:
+			gracz.take_dmg(10, 1.1, m.global_position)
+			$miniTimer.start()
+			swap = m
+		else:
+			add_child(m)
+			m.global_position = pozycjaMacki
 
 func dmg(dmg): #przyjmowanie obrażeń od ciosów w macki
 	hp -= dmg
@@ -41,7 +54,21 @@ func die():
 	p.global_position = get_node("../..").global_position #dokładnie na środku pokoju
 	level.add_child(p)
 	self.queue_free()
+	rng.randomize() # losowanie generatora liczb
+	var coins = rng.randf_range(drop['minCoins'], drop["maxCoins"]) # wylosowanie ilości coinsów
+	for i in range (0, coins): # pętla tworząca monety
+		randomPosition = Vector2(rng.randf_range(self.global_position.x-10,self.global_position.x+10),rng.randf_range(self.global_position.y-10,self.global_position.y+10)) # precyzowanie losowej pozycji monet
+		var coin = load("res://Scenes/Loot/GoldCoin.tscn") # zmienna coin to odwołanie do sceny GoldCoin.tscn
+		coin = coin.instance() # coin staje się nową instacją coina
+		coin.position = randomPosition # pozycją coina jest wylosowana wcześniej pozycja
+		level.add_child(coin) # coin jest dzieckiem level
 
 func _on_Wzrok_body_entered(body):
 	if body.name == "Player":
 		gracz = body
+
+func _on_miniTimer_timeout():
+	if swap:
+		add_child(swap)
+		swap.global_position = pozycjaMacki
+		swap = null
--- Scenes/Actors/OctoBoss/tentacle.gd
+++ Scenes/Actors/OctoBoss/tentacle.gd
@@ -1,11 +1,10 @@
 extends KinematicBody2D
 
 var floating_dmg = preload("res://Scenes/UI/FloatingDmg.tscn") # wizualny efekt zadanych obrażeń
-var gracz
-var u = 10 #macki nie posiadają określonej ilości HP
-#po przyjęciu u ciosów chowają się
 
 func _ready():
+	$AnimationPlayer.play("intro")
+	yield($AnimationPlayer,"animation_finished")
 	$AnimationPlayer.play("idle")
 
 func get_dmg(dmg, weaponKnockback):
@@ -16,18 +15,8 @@ func get_dmg(dmg, weaponKnockback):
 	text.type = "Damage"
 	add_child(text)
 	# == =============== ==
-	u -= 1
-	if (u <= 0):
-		self.queue_free() #po u ciosach macka znika
-
-func _on_Zasieg_body_entered(body):
-	if body.name == "Player":
-		gracz = body
-
-func _on_Zasieg_body_exited(body):
-	if body.name == "Player":
-		gracz = null
 
 func _on_Timer_timeout():
-	if gracz:
-		gracz.take_dmg(20, 0.7, self.global_position)
+	$AnimationPlayer.play("outro")
+	yield($AnimationPlayer,"animation_finished")
+	self.queue_free()
