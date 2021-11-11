ad6cdb48fc2853dbc147a4d01300e350f2c8cd1d
DominikaMa3ur <169584@stud.prz.edu.pl>
2021-11-11 15:25:40 +0100

    Ośmiornica
    + zmiana mechaniki: po trafieniu w gracza macka chowa się pod ziemię, można bić tylko w macki, których uderzenia się uniknęło (naprawia problem blokowania się gracza przy braku konieczności ograniczania liczby macek)
    + komentarze do kodu
    + stylizowany pasek HP

--- Scenes/Actors/OctoBoss/tentacle.gd
+++ Scenes/Actors/OctoBoss/tentacle.gd
@@ -1,14 +1,20 @@
 extends KinematicBody2D
 
 var floating_dmg = preload("res://Scenes/UI/FloatingDmg.tscn") # wizualny efekt zadanych obrażeń
+var uderzyla = false
 
 func _ready():
 	$AnimationPlayer.play("intro")
 	yield($AnimationPlayer,"animation_finished")
-	$AnimationPlayer.play("idle")
+	if uderzyla: # macka, która trafiła w gracza, chowa się po udanym ataku
+		$AnimationPlayer.play("outro2")
+		yield($AnimationPlayer,"animation_finished")
+		self.queue_free()
+	else:
+		$AnimationPlayer.play("idle")
 
 func get_dmg(dmg, weaponKnockback):
-	get_parent().dmg(dmg) #obrażenia są odejmowane od zdrowia głównego bossa
+	get_parent().dmg(dmg) # obrażenia są odejmowane od zdrowia głównego bossa
 	# == EFEKTY WIZUALNE ==
 	var text = floating_dmg.instance()
 	text.amount = dmg
@@ -16,7 +22,7 @@ func get_dmg(dmg, weaponKnockback):
 	add_child(text)
 	# == =============== ==
 
-func _on_Timer_timeout():
+func _on_Timer_timeout(): # po kilku sekundach macka chowa się pod ziemię
 	$AnimationPlayer.play("outro")
 	yield($AnimationPlayer,"animation_finished")
 	self.queue_free()
