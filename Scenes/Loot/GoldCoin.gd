e00359a819032972539b02bfcb7b0c83f7d59d6a
Maksymilian Dendura <maksd0410@gmail.com>
2021-11-11 16:28:12 +0100

    Dodanie losowej szybkości wypadania

--- Scenes/Loot/GoldCoin.gd
+++ Scenes/Loot/GoldCoin.gd
@@ -4,10 +4,13 @@ var rng = RandomNumberGenerator.new()
 
 func _ready():
 	$AnimationPlayer.play("Idle") #Odtwarzaj animację obracania
-	#Wylosowanie liczby i odtworzenie animacji wyapadnia w lewo lub prawo
 	rng.randomize()
-	var random_number = rng.randi_range(0, 1)
-	if (random_number == 0):
+	#Wylosowanie szybkości wypadania coina
+	var random_speed = rng.randf_range(0.9, 1.6)
+	$AnimationPlayer2.playback_speed = random_speed
+	#Wylosowanie kierunku wypadania coina
+	var random_direction = rng.randi_range(0, 1)
+	if (random_direction == 0):
 		$AnimationPlayer2.play("Drop-left")
 	else:
-		$AnimationPlayer2.play("Drop-right") 
+		$AnimationPlayer2.play("Drop-right")
