52eb61675bfc3c3e7bf8785a96b523916270226b
Arek Strugała <arkadiuszstrugala0@gmail.com>
2021-11-15 15:27:43 +0100

    dodane dwie umiejetności
    Pierwsza umiejetność to rzut toporem a druga coś jak ma olaf w lol'u czyli odporny na negatywne efekty zwiekszona predkosc poruszania się atak speed wiekszy, dmg zwiekszone

--- Scenes/Equipment/Weapons/Melee/Axe.gd
+++ Scenes/Equipment/Weapons/Melee/Axe.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var player_node = get_tree().get_root().find_node("Player", true, false)
+var StatusBar_node = get_tree().get_root().find_node("StatusBar", true, false)
 var mouse_position #Pozycja kursora
 var attack = false #Czy postać atakuje
 var attack_vector = Vector2.ZERO #Wektor po którym porusza się broń podczas ataku
@@ -15,6 +17,25 @@ var attack_speed = 0
 var swing_to = 0.2
 var swing_back = 0.4
 var animation_step = 0.01
+
+#do um1
+var active_ability1 = false
+var active_ability2 = false
+var ability1rotation = 530
+var ability1range = 10
+var tmpdmg
+var tmpknockback
+var ability1damagemultipler = 10
+
+#do um2
+var ability2healamount = 50
+var tmpspeed
+var tmpattack_speed
+var speedmultipler = 1.5
+var dmgmultipler = 2
+var attack_speedmultipler = 2
+var ability2duration = 5
+
 
 func _physics_process(delta):
 	if a:#Zmienia ustawienia timera i teksturę a także skaluje kolizję (_ready() nie działa)
@@ -44,10 +65,43 @@ func _physics_process(delta):
 
 
 	if Input.is_action_just_pressed("use_ability_1"):
-		var placeholder5 #kilkanie przycisku działa abilitki trza zrobić
-	if Input.is_action_just_pressed("use_ability_2"):
-		var palceholder20
+		if active_ability1==false and active_ability2==false and player_node.mana>=25:
+			if (player_node.current_weapon==1 and !player_node.get_node("CoolDownS1").get_time_left()) or (player_node.current_weapon==2 and !player_node.get_node("CoolDownS3").get_time_left()):
+				player_node.on_skill_used(1,25)
+				attack = true
+				active_ability1 = true
+				$AttackCollision.disabled = false
+				attack_vector = Vector2(attack_range *ability1range* cos(rotation), attack_range *ability1range* sin(rotation))
+				tmpdmg = damage
+				damage *= ability1damagemultipler
+				tmpknockback = weaponKnockback
+				weaponKnockback = 0
+				timer.start()
 		
+		
+	if Input.is_action_just_pressed("use_ability_2"):
+		if active_ability1==false and active_ability2==false and player_node.mana>=50:
+			if (player_node.current_weapon==1 and !player_node.get_node("CoolDownS2").get_time_left()) or (player_node.current_weapon==2 and !player_node.get_node("CoolDownS4").get_time_left()):
+				player_node.on_skill_used(2,50)
+				StatusBar_node.immune = true
+				tmpspeed = player_node.speed
+				player_node.speed *= speedmultipler
+				tmpdmg = damage
+				damage *= dmgmultipler
+				tmpattack_speed = attack_speed
+				attack_speed *= attack_speedmultipler
+				
+				var t = Timer.new()   			
+				t.set_wait_time(ability2duration)
+				t.set_one_shot(true)			
+				self.add_child(t)				
+				t.start()						
+				yield(t, "timeout")
+				
+				player_node.speed = tmpspeed
+				damage = tmpdmg
+				attack_speed = tmpattack_speed
+				StatusBar_node.immune = false
 		
 func reset_pivot(): #Zresetuj broń. Nawet jak animacja jest spieprzona to broń nie oddali się od gracza
 	position.x=0.281
@@ -65,15 +119,25 @@ func _on_Timer_timeout():#Wykonuje się kiedy zejdzie cooldown ataku
 	if attack_speed <= swing_to:
 		position += attack_vector * (animation_step/swing_to)
 		if rotation < -PI/2 or rotation > PI/2:
-			$WeaponSprite.rotation_degrees += -90 * (animation_step/swing_to)
+			if active_ability1 == true:
+				$WeaponSprite.rotation_degrees += -ability1rotation * (animation_step/swing_to)
+			else:
+				$WeaponSprite.rotation_degrees += -90 * (animation_step/swing_to)
 		else:
-			$WeaponSprite.rotation_degrees += 90 * (animation_step/swing_to)
+			if active_ability1 == true:
+				$WeaponSprite.rotation_degrees += ability1rotation * (animation_step/swing_to)
+			else:
+				$WeaponSprite.rotation_degrees += 90 * (animation_step/swing_to)
 		
 	elif attack_speed > swing_back:
 		position -= attack_vector
 		$WeaponSprite.rotation_degrees = 0
 		$AttackCollision.disabled = true
 		attack = false
+		if active_ability1 == true:
+			damage = tmpdmg
+			weaponKnockback = tmpknockback
+		active_ability1 = false
 		attack_speed = 0
 		timer.stop()
 		reset_pivot()
