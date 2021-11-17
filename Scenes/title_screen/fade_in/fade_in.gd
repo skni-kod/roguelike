9529108b86be03cb41f17ff1dbabc074489e8659
Artur Szałyga <arturszalyga@wp.pl>
2021-11-17 04:26:29 +0100

    Menu Visual Effects

--- Scenes/title_screen/fade_in/fade_in.gd
+++ Scenes/title_screen/fade_in/fade_in.gd
@@ -3,6 +3,7 @@ extends ColorRect
 signal fade_finished		#kod do efekty przcyemnienia pomiędzy scenami
 
 func fade_in():
+	get_parent().get_node("Lights/Lamp_2").visible = false
 	$AnimationPlayer.play("fade_in")#odpala efekt
 	
 func _on_AnimationPlayer_animation_finished(anim_name):#daje sygnał że efekt się skończył
