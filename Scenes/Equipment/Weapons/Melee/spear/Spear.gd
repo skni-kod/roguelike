51167ac625a74d4f10f8440437bae66ebb57a0b2
Smok7kp <43822731+Smok7kp@users.noreply.github.com>
2022-03-31 18:55:21 +0200

    ability 1

--- Scenes/Equipment/Weapons/Melee/spear/Spear.gd
+++ Scenes/Equipment/Weapons/Melee/spear/Spear.gd
@@ -18,6 +18,8 @@ var crit_chance = rng.randi_range(0,10)
 var crit = false
 var crit_damage = 2
 
+#for passive 
+var combo=1
 #do um1
 
 #do um2
@@ -26,7 +28,7 @@ func _ready() -> void:
 	damage = float(Weapons.all_weapons.Axe["attack"])
 	weaponKnockback = float(Weapons.all_weapons.Axe["knc"])
 	attack_speed = float(Weapons.all_weapons.Axe["spd"])
-	$AnimationPlayer.play("RESET")
+	$WeakAttack.play("RESET")
 
 
 func _physics_process(_delta):
@@ -35,27 +37,56 @@ func _physics_process(_delta):
 
 
 func _unhandled_input(event) -> void:
+
 	if !attacking:
 		if Input.is_action_just_pressed("attack"):
 			print("[INFO]: Event identified as BUTTON_LEFT pressed")
-			$AnimationPlayer.playback_speed = attack_speed
-			$AnimationPlayer.play("Attack")
-			yield($AnimationPlayer, "animation_finished")
-			$AnimationPlayer.play("RESET")
-			
+			if combo == 1:
+				$WeakAttack.playback_speed = attack_speed
+				$WeakAttack.play("Attack")
+				yield($WeakAttack, "animation_finished")
+				$WeakAttack.play("RESET")
+				combo=2
+				$ComboTimer.start()
+			elif combo == 2 and $ComboTimer.get_time_left()>0:
+				$WeakAttack.playback_speed = attack_speed
+				$WeakAttack.play("Attack")
+				yield($WeakAttack, "animation_finished")
+				$WeakAttack.play("RESET")
+				combo=3
+				$ComboTimer.start()
+			elif combo == 3 and $ComboTimer.get_time_left()>0:
+				$StrongAttack.playback_speed = attack_speed
+				$StrongAttack.play("Attack")
+				yield($StrongAttack, "animation_finished")
+				$StrongAttack.play("RESET")
+				combo=1
+			else: 
+				combo=1 
+
 		if event.is_action_pressed("use_ability_1"):
 			if Player_node.mana>=25:
 				if (Player_node.equippedWeapons[1]==weaponName and !Player_node.get_node("CoolDownS1").get_time_left()) or (Player_node.equippedWeapons[2]==weaponName and !Player_node.get_node("CoolDownS3").get_time_left()): #if sprawdzający czy nie ma cooldownu na umce
-					Player_node.start_skill_cooldown(1, 5, 30)
+					Player_node.start_skill_cooldown(1, 10, 20)
 					spell = 1
+					weaponKnockback = weaponKnockback*4
+					for i in 2:
+						$RepelAttack.playback_speed = attack_speed*2
+						$RepelAttack.play("Attack")
+						yield($RepelAttack, "animation_finished")
+						$RepelAttack.play("RESET")
+					weaponKnockback = weaponKnockback/4
 					spell = 0
 		elif event.is_action_pressed("use_ability_2"):
 			if Player_node.mana>=50:
 				if (Player_node.equippedWeapons[1]==weaponName and !Player_node.get_node("CoolDownS2").get_time_left()) or (Player_node.equippedWeapons[2]==weaponName and !Player_node.get_node("CoolDownS4").get_time_left()):
 					Player_node.start_skill_cooldown(2, 50, 50)
 					spell = 1
+					
 					spell = 0
 
+func _on_ComboTimer_timeout() -> void:
+	combo = 1
 
 func _on_Player_attacked() -> void:
 	if !attacking: #Sprawdza czy broń nie jest w trakcie ataku
@@ -95,3 +126,6 @@ func set_attack_state(value: bool) -> void:
 func get_attack_state() -> bool:
 	return attacking
 # === ============================ === #
+
+
+
