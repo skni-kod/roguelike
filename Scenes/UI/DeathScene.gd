b8c3c01cf854deb80f95bf935e782c5ee7694e5c
Artur Szałyga <arturszalyga@wp.pl>
2022-04-03 22:38:46 +0200

    DeathScene now works properly

--- Scenes/UI/DeathScene.gd
+++ Scenes/UI/DeathScene.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
-func _physics_process(delta):
-	if $CanvasLayer/Button.pressed:
-		get_tree().change_scene("res://Scenes/title_screen/TitleScreen.tscn")
+
+func _on_Button_pressed() -> void:
+	MusicController.stop_music()
+	MusicController.play_menu_music()
+	get_tree().change_scene("res://Scenes/title_screen/TitleScreen.tscn")
