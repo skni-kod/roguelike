ba2a58c8a2b5b128eaed2a04d2fd004dc7a8a96a
Artur Szałyga <arturszalyga@wp.pl>
2022-04-03 23:53:50 +0200

    sounds added, playersprite fixed

--- Scenes/UI/DeathScene.gd
+++ Scenes/UI/DeathScene.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 
+func _ready() -> void:
+	$CanvasLayer/Button.grab_focus()
+
+
 func _on_Button_pressed() -> void:
 	MusicController.stop_music()
 	MusicController.play_menu_music()
